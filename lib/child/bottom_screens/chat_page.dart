6e75dc5db5ebf8be38d50cc15da6289e10d85872
Miftah Rahi <miftahrahi24@gmail.com>
2023-06-14 16:01:31 +0600

    Initial Commit

---
+++ lib/child/bottom_screens/chat_page.dart
@@ -0,0 +1,78 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:women_safety_app/chat_module/chat_screen.dart';
+import 'package:women_safety_app/child/child_login_screen.dart';
+import 'package:women_safety_app/db/share_pref.dart';
+
+import '../../utils/constants.dart';
+
+class ChatPage extends StatefulWidget {
+  const ChatPage({super.key});
+
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  @override
+  void initState() {
+    super.initState();
+
+    if (FirebaseAuth.instance.currentUser!.uid.isEmpty) {
+      Navigator.push(context, MaterialPageRoute(builder: (_) => LoginScreen()));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.pink,
+        // backgroundColor: Color.fromARGB(255, 250, 163, 192),
+        title: Text("SELECT GUARDIAN"),
+      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('users')
+            .where('type', isEqualTo: 'parent')
+            .where('childEmail',
+                isEqualTo: FirebaseAuth.instance.currentUser!.email)
+            .snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData) {
+            return Center(child: progressIndicator(context));
+          }
+          return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (BuildContext context, int index) {
+              final d = snapshot.data!.docs[index];
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  color: Color.fromARGB(255, 250, 163, 192),
+                  child: ListTile(
+                    onTap: () {
+                      goTo(
+                          context,
+                          ChatScreen(
+                              currentUserId:
+                                  FirebaseAuth.instance.currentUser!.uid,
+                              friendId: d.id,
+                              friendName: d['name']));
+                      // Navigator.push(context, MaterialPa)
+                    },
+                    title: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(d['name']),
+                    ),
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:women_safety_app/db/share_pref.dart';
+//import 'package:women_safety_app/child/bottom_screens/child_home_page.dart';
+// import 'package:women_safety_app/child/child_login_screen.dart';
+// import 'package:women_safety_app/parent/parent_home_screen.dart';
+// import 'package:women_safety_app/utils/constants.dart';
+import 'package:women_safety_app/utils/flutter_background_services.dart';
+import 'child/bottom_page.dart';
+
+final navigatorkey = GlobalKey<ScaffoldMessengerState>();
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  await MySharedPrefference.init();
+  await initializeService();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        // scaffoldMessengerKey: navigatorkey,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          textTheme: GoogleFonts.firaSansTextTheme(
+            Theme.of(context).textTheme,
+          ),
+          primarySwatch: Colors.blue,
+        ),
+        home: BottomPage());
+  }
+}
+
+// class CheckAuth extends StatelessWidget {
+//   // const CheckAuth({Key? key}) : super(key: key);
+
+//   checkData() {
+//     if (MySharedPrefference.getUserType() == 'parent') {}
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold();
+//   }
+// }
